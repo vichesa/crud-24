4068e38ca99f5ecf900b1e0b1ea9e927a7b01f17
Your Name <you@example.com>
2021-08-09 08:18:34 +0700

    revisi add delete

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -1,7 +1,22 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function DataTable(props) {
   const { post } = props;
+
+  const deleteNote = (id) => {
+    window.location.reload();
+    // <-- declare id parameter
+    Axios.delete(`http://localhost:3002/api/delete/${id}`)
+      .then((res) => {
+        console.log("deleted", res);
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <tr key={post.id}>
       <th scope="row">{post.id}</th>
@@ -13,7 +28,17 @@ export default function DataTable(props) {
           : post.post_text}
       </td>
       <td scope="row">
-        <a href={`/post/${post.id}`}>Edit</a>
+        <a className="btn btn-success p-1 me-1" href={`/post/${post.id}`}>
+          {" "}
+          <i className="far fa-edit"></i>{" "}
+        </a>
+        <button
+          className="btn btn-danger p-1"
+          onClick={() => deleteNote(post.id)}
+        >
+          {" "}
+          <i className="fas fa-trash"></i>
+        </button>
       </td>
     </tr>
   );
